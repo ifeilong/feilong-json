1a5ef2cc0fb02b15089265b820ef72e7e4da64ac
feilong <venusdrogon@163.com>
2018-04-11 16:42:36 +0800

    :memo: update javadoc

--- feilong-json-jsonlib/src/main/java/com/feilong/json/jsonlib/builder/ArrayExcludePropertyNamesPropertyFilter.java
+++ feilong-json-jsonlib/src/main/java/com/feilong/json/jsonlib/builder/ArrayExcludePropertyNamesPropertyFilter.java
@@ -19,8 +19,82 @@ import net.sf.json.util.PropertyFilter;
 
 /**
  * 排除属性过滤器.
+ * 
+ * json 转java 的时候,想排除某个属性
+ * 
+ * 比如 taobao 接口返回的数据, 有老的废弃属性 county (可能早期由于单词写错了. 为了兼容老代码)
+ * 
+ * <pre>
+ * {@code
+ * {
+ *             "country": "中国",
+ *             "country_id": "CN",
+ *             "area": "华北",
+ *             "area_id": "100000",
+ *             "region": "北京市",
+ *             "region_id": "110000",
+ *             "city": "北京市",
+ *             "city_id": "110100",
+ *             "county": "",
+ *             "county_id": "-1",
+ *             "isp": "科技网",
+ *             "isp_id": "1000114",
+ *             "ip": "210.75.225.254"
+ *         }
+ * }
+ * </pre>
+ * 
+ * 
+ * 但是我们的java bean 没有这个属性
+ * 
+ * 导致转换的时候, 由于 使用了 PropertyStrategyWrapper
+ * 
+ * <pre>
+ * {@code
+ *   public void setProperty(Object bean,String key,Object value){
+ *         try{
+ *             propertySetStrategy.setProperty(bean, key, value);
+ *         }catch (Exception e){
+ *             LOGGER.warn(e.getMessage(), e);
+ *         }
+ *     }
+ * }
+ * </pre>
+ * 
+ * 会提示 warn 级别的日志, 很烦人
+ * 
+ * <pre>
+ * {@code
+ * 
+ * 20:33:38 WARN  (PropertyStrategyWrapper.java:69) [setProperty()] java.lang.NoSuchMethodException: Unknown property 'county' on class 'class com.feilong.tools.IpInfoEntity'
+ * net.sf.json.JSONException: java.lang.NoSuchMethodException: Unknown property 'county' on class 'class com.feilong.tools.IpInfoEntity'
+ *     at net.sf.json.util.PropertySetStrategy$DefaultPropertySetStrategy._setProperty(PropertySetStrategy.java:69)
+ *     at net.sf.json.util.PropertySetStrategy$DefaultPropertySetStrategy.setProperty(PropertySetStrategy.java:60)
+ *     at net.sf.json.util.PropertySetStrategy$DefaultPropertySetStrategy.setProperty(PropertySetStrategy.java:45)
+ *     at com.feilong.json.jsonlib.PropertyStrategyWrapper.setProperty(PropertyStrategyWrapper.java:67)
+ *     at net.sf.json.util.PropertySetStrategy.setProperty(PropertySetStrategy.java:40)
+ *     at net.sf.json.JSONObject.setProperty(JSONObject.java:1394)
+ *     at net.sf.json.JSONObject.toBean(JSONObject.java:429)
+ *     at com.feilong.json.jsonlib.JsonUtil.toBean(JsonUtil.java:1275)
+ *     at com.feilong.tools.IpUtil.toIpInfoEntity(IpUtil.java:162)
+ *     at com.feilong.tools.IpUtil.getIpInfoEntity(IpUtil.java:138)
+ *     at com.feilong.tools.IpUtilTest.test(IpUtilTest.java:36)
+ *     at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:57)
+ *     at org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)
+ *     at org.junit.runners.ParentRunner.run(ParentRunner.java:363)
+ *     at org.eclipse.jdt.internal.junit4.runner.JUnit4TestReference.run(JUnit4TestReference.java:86)
+ *     at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.run(RemoteTestRunner.java:460)
+ *     at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.main(RemoteTestRunner.java:206)
+ * Caused by: java.lang.NoSuchMethodException: Unknown property 'county' on class 'class com.feilong.tools.IpInfoEntity'
+ *     at org.apache.commons.beanutils.PropertyUtilsBean.setSimpleProperty(PropertyUtilsBean.java:2091)
+ *     at org.apache.commons.beanutils.PropertyUtils.setSimpleProperty(PropertyUtils.java:928)
+ *     at net.sf.json.util.PropertySetStrategy$DefaultPropertySetStrategy._setProperty(PropertySetStrategy.java:67)
+ *     ... 33 more
+ * }
+ * </pre>
  *
  * @author <a href="http://feitianbenyue.iteye.com/">feilong</a>
+ * @see <a href="https://github.com/venusdrogon/feilong-json/issues/3">json 转java 的时候,想排除某个属性</a>
  * @since 1.11.0
  */
 class ArrayExcludePropertyNamesPropertyFilter implements PropertyFilter{
